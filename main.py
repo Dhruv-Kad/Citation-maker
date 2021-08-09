8730b5d423a58573a995af3cbbec8b01d0932ed8
dhruv <kadamdhruv98@gmail.com>
2021-08-09 17:32:41 -0500

    Changed whole idea so now all you need to is enter in a url

--- main.py
+++ main.py
@@ -3,44 +3,23 @@ import requests
 import webbrowser
 import pprint
 
-urlstring = []
-new_list = []
-lol = input("Enter in search terms: ")
-url = "https://www.google.com/search?q=" + lol
-searchdict = {}
-compdict = {}
-response = requests.get(url)
-k = 1
-with open('output.html', 'wb') as f:
-    f.write(response.content)
-webbrowser.open('output.html')
+url = input("Enter url: ")
+req = requests.get(url)
+webbrowser.open(url)
+soup = BeautifulSoup(req.text, 'lxml')
+dls = soup.find_all("dl",{'class':'si_row'})
+for dl in dls:
+    atag = dl.find('a')
+    if atag:
+        author_link = atag.get('href')
+        author_name = atag.get_text()
+        print(author_link)
+        print(author_name)
 
-
-soup = BeautifulSoup(response.text, 'lxml')
-for tag in soup.find_all(class_="BNeawe vvjwJb AP7Wnd"):
-    searchdict[k] = tag.text
-    k += 1
-k = 1
-for a in soup.find_all('a'):
-    urlstring.append(a.get_attribute_list('href')[0])
-for x in urlstring:
-    if "/url?q=" in x:
-        compdict[k] = x
-        k += 1
-
-print(new_list)
-
-pprint.pprint(searchdict)
-pprint.pprint(compdict)
-
-
-
-
-cite = int(input("Enter in the number of the link you want: "))
-
-print(searchdict[cite])
-now = (compdict[cite])
-newrec = "https://www.google.com/search?q=" + now
+span_date = soup.find('span',{'class':'item_date'})
+if span_date:
+    date = span_date.get_text()
+    print(date)
 
 
 
